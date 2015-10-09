df6d08c3801b8a7fc924a6735b77aae0d13dd783
mdaif (mohamed.daif) <muhammad.daif@gmail.com>
2015-10-09 17:28:06 +0200

    adds update method

--- descriptors.py
+++ descriptors.py
@@ -38,8 +38,6 @@ class PositiveFloat(Float, Positive):
 
 class HttpModelMeta(type):
 	def _create(self, **params):
-
-
 		for k, v in params.items():
 			setattr(self, k, v)
 
@@ -47,7 +45,14 @@ class HttpModelMeta(type):
 		body = json.dumps(self.__dict__)
 		return requests.post(create_endpoint, data=body)
 
+	def _update(self, **params):
+		self.__dict__ = {}
+		for k, v in params.items():
+			setattr(self, k, v)
 
+		update_endpoint = self.Meta.update
+		body = json.dumps(self.__dict__)
+		return requests.put(update_endpoint, data=body)
 
 	def __new__(cls, clsname, bases, clsdict):
 		for name, value in clsdict.items():
@@ -56,6 +61,7 @@ class HttpModelMeta(type):
 
 		new_class = type.__new__(cls, clsname, bases, clsdict)
 		setattr(new_class, "create", cls._create)
+		setattr(new_class, "update", cls._update)
 		return new_class
 
 class HttpModel(metaclass=HttpModelMeta):
@@ -77,6 +83,7 @@ class Student(HttpModel):
 
 	class Meta:
 		create = "http://www.mocky.io/v2/5617b761100000132472226c"
+		update = "http://www.mocky.io/v2/5617b761100000132472226c"
 
 if __name__ == "__main__":
 	stock = Stock()
@@ -84,5 +91,11 @@ if __name__ == "__main__":
 	print(create_response.text)
 
 	student = Student()
+	print("1", student.__dict__)
 	create_student = student.create(name='7amada', age=18, gpa=3.6)
+	print("2", student.__dict__)
 	print(create_student.json())
+
+	update_student = student.update(name='7amboozo', age=19)
+	print("3", student.__dict__)
+	print(update_student.json())
