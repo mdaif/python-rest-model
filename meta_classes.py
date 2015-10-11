b7508e5c6047cd1e408e9601208654eb928788ad
mdaif (mohamed.daif) <muhammad.daif@gmail.com>
2015-10-11 15:44:47 +0200

    adds initial implementation for get, delete

--- meta_classes.py
+++ meta_classes.py
@@ -2,32 +2,54 @@ from primitives import Typed
 import requests
 import json
 
+def _post(self, **params):
+    self.__dict__ = {}
+
+    for k, v in params.items():
+        setattr(self, k, v)
+
+    body = json.dumps(self.__dict__)
+    return requests.post(self.Meta.post, data=body)
+
+def _put(self, id, **params):
+    self.__dict__ = {}
+    setattr(self, 'id', id)
+    for k, v in params.items():
+        setattr(self, k, v)
+
+    body = json.dumps(self.__dict__)
+    return requests.put(self.Meta.put, data=body)
+
+def _delete(self, id):
+    return requests.delete(self.Meta.delete)
+
+def _get(self, id=None, **params):
+    """if get has an id it should be used to retrieve a single item otherwise it would be retrieving a list"""
+
+    self.__dict__ = {}
+    if id is not None:
+        setattr(self, 'id', id)
+        # do some string interpolation to the endpoint
+    for k, v in params.items():
+        setattr(self, k, v)
+
+    body = json.dumps(self.__dict__)
+    return requests.put(self.Meta.get, data=body)
+
+
+
 
 class RestModelMeta(type):
-    def _create(self, **params):
-        for k, v in params.items():
-            setattr(self, k, v)
-
-        create_endpoint = self.Meta.create
-        body = json.dumps(self.__dict__)
-        return requests.post(create_endpoint, data=body)
-
-    def _update(self, **params):
-        self.__dict__ = {}
-        for k, v in params.items():
-            setattr(self, k, v)
-
-        update_endpoint = self.Meta.update
-        body = json.dumps(self.__dict__)
-        return requests.put(update_endpoint, data=body)
-
     def __new__(cls, clsname, bases, clsdict):
         for name, value in clsdict.items():
             if isinstance(value, Typed):
                 value._name = name
 
         new_class = type.__new__(cls, clsname, bases, clsdict)
-        setattr(new_class, "create", cls._create)
-        setattr(new_class, "update", cls._update)
+
+        setattr(new_class, "post", _post)
+        setattr(new_class, "put", _put)
+        setattr(new_class, "delete", _delete)
+        setattr(new_class, "get", _get)
 
         return new_class
--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class Stock(models.RestModel):
 	price = models.PositiveFloatField()
 
 	class Meta:
-		create = "http://www.mocky.io/v2/5185415ba171ea3a00704eed/"
+		post = "http://www.mocky.io/v2/5185415ba171ea3a00704eed/"
 
 
 class Student(models.RestModel):
@@ -16,21 +16,32 @@ class Student(models.RestModel):
 	gpa = models.PositiveFloatField()
 
 	class Meta:
-		create = "http://www.mocky.io/v2/5617b761100000132472226c"
-		update = "http://www.mocky.io/v2/5617b761100000132472226c"
+		post = "http://www.mocky.io/v2/561a5fe6100000a32068d55f"
+		put = "http://www.mocky.io/v2/561a604c100000a32068d560"
+		delete = "http://www.mocky.io/v2/561a616f100000ef2068d561"
+		get = "http://www.mocky.io/v2/561a5f0a100000872068d55e"
 
 
 if __name__ == "__main__":
 	stock = Stock()
-	create_response = stock.create(name='daif', shares=3, price=2.2)
+	create_response = stock.post(name='IBM', shares=3, price=2.2)
 	print(create_response.text)
 
 	student = Student()
 	print("1", student.__dict__)
-	create_student = student.create(name='7amada', age=18, gpa=3.6)
+
+	create_student = student.post(name='Daif', age=18, gpa=3.6)
 	print("2", student.__dict__)
 	print(create_student.json())
 
-	update_student = student.update(name='7amboozo', age=19)
+	update_student = student.put(10234, gpa=4.0)
 	print("3", student.__dict__)
 	print(update_student.json())
+
+	get_student = student.get()
+	print("4", student.__dict__)
+	print(get_student.json())
+
+	delete_student = student.delete(10234)
+	print("5", student.__dict__)
+	print(delete_student.status_code)
